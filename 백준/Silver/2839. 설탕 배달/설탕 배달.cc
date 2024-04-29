91d311ef726ee0cf0c198815453e2594d673c3aa
Janggihyeon <144190282+Janggihyeon@users.noreply.github.com>
2024-04-29 15:44:42 +0900

    [Silver IV] Title: 설탕 배달, Time: 4 ms, Memory: 99680 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    int number = 0;
+    int i = 0;
+    bool check=false;
+    int min = 10000;
+    cin >> number;
+    int* total = new int[number*number];
+    for (int x = 0; x <= number / 5; x++)
+    {
+        for(int y=0;y<=(number-5*x)/3;y++)
+            if (5 * x + 3 * y == number)
+            {
+                total[i++] = x + y;
+            }
+            else
+            {
+                total[i++] = -1;
+            }
+    }
+    for (int j = 0; j < i; j++)
+    {
+        if (min > total[j] && total[j] > -1)
+        {    min = total[j];
+             check=true;
+        }
+    }
+    if(!check)
+        min=-1;
+    
+    cout << min << endl;
+    return 0;
+}
